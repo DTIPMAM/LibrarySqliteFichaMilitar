659f927a2989f131f4a7d72d8ee3c360a953b845
maycon medeiros <maycon1038@gmail.com>
2025-07-28 11:26:29 -0400

    Remove Firebase Crashlytics e atualiza texto do Snackbar
    - Remove a dependência `firebase-crashlytics-gradle` do arquivo `build.gradle` raiz.
    - Remove o plugin `com.google.firebase.crashlytics` do arquivo `librarysqlitefichamilitar/build.gradle`.
    - Remove a dependência de teste `junit` do arquivo `app/build.gradle`.
    - Altera o texto do Snackbar em `MainActivity.java` para "Substitua por sua própria ação" e a ação para "ação".

--- app/src/main/java/com/example/librarysqiltefichamilitar/MainActivity.java
+++ app/src/main/java/com/example/librarysqiltefichamilitar/MainActivity.java
@@ -25,8 +25,8 @@ public class MainActivity extends AppCompatActivity {
 		fab.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-					.setAction("Action", null).show();
+				Snackbar.make(view, "Substitua por sua própria ação", Snackbar.LENGTH_LONG)
+					.setAction("ação", null).show();
 			}
 		});
 	}
